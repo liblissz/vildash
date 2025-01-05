d14e70ec88eb521f40fb03e7f275839db0965895
liblissz <liblissz3@gmail.com>
2025-01-05 15:32:48 -0800

    Add files via upload

---
+++ c/random.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    char grade = "A";
+    float pi = 3.14159;
+    double avogadro = 6.02214076e23;
+
+    printf("Age: %d\n", age);
+    printf("Grade: %c\n", grade);
+    printf("Pi: %f\n", pi);
+    printf("Avogadro's number: %lf\n", avogadro);
+
+    return 0;
+}
